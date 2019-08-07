69d5935932bc1e3ef81e9b143d358debd066c5ae
Baljeet Singh <bsinghs.007@gmail.com>
2019-08-07 11:50:28 +0530

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const options = new DocumentBuilder()
     .setTitle('Quotes Api')
     .setDescription('Quotes API Description')
