54ed1896d75ac002de669014a49131831bbcbe4d
Baljeet Singh <bsinghs.007@gmail.com>
2019-08-07 12:23:49 +0530

    adding quotes module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuotesController } from './quotes/quotes.controller';
-import { QuotesService } from './quotes/quotes.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { QuotesModule } from './quotes/quotes.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI, {
       useNewUrlParser: true,
     }),
+    QuotesModule,
   ],
-  controllers: [AppController, QuotesController],
-  providers: [AppService, QuotesService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
