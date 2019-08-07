b08661cd2459daf888217d9189d439429307a263
Baljeet Singh <bsinghs.007@gmail.com>
2019-08-07 11:59:31 +0530

    connecting to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuotesController } from './quotes/quotes.controller';
 import { QuotesService } from './quotes/quotes.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/quotes', {
+      useNewUrlParser: true,
+    }),
+  ],
   controllers: [AppController, QuotesController],
   providers: [AppService, QuotesService],
 })
